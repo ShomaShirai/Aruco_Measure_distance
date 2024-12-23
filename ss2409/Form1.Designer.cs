7c411afb77d59c1668a7ba3da65097d6defabdaa
ShomaShirai <22t0837a@student.gs.chiba-u.jp>
2024-12-23 17:32:55 +0900

    プロジェクト ファイルを追加します。

---
+++ ss2409/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ss2409
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.directoryEntry2 = new System.DirectoryServices.DirectoryEntry();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.tabControl1.Location = new System.Drawing.Point(17, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1074, 739);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.comboBox2);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 34);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1066, 701);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "キャリブレーションモード";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(966, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 24);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "フレーム";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "3",
+            "5",
+            "10"});
+            this.comboBox2.Location = new System.Drawing.Point(809, 276);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 32);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(807, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "撮影間隔";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(956, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "枚";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(806, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "撮影枚数";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label1.Location = new System.Drawing.Point(42, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(764, 30);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "指示棒でPivot操作の準備ができたら開始ボタンを押してください";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "15",
+            "20",
+            "25",
+            "30"});
+            this.comboBox1.Location = new System.Drawing.Point(809, 137);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 32);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(813, 427);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 84);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "開始/中断";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 77);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(731, 572);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 34);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1066, 701);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "計測モード";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 777);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.DirectoryServices.DirectoryEntry directoryEntry2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ ss2409/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Aruco;
+using OpenCvSharp.Extensions;
+
+
+namespace ss2409
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _videoCapture;
+        private bool _isCameraRunning = false;
+        //private Mat _frame;
+        private Dictionary _dictionary;
+        private DetectorParameters _detectorParameters;
+        private int _captureNumber = 20;
+        private int _captureInterval = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAruco();
+        }
+
+        // 初期化: Aruco辞書と検出パラメータを設定
+        private void InitializeAruco()
+        {
+            _dictionary = CvAruco.GetPredefinedDictionary(PredefinedDictionaryName.Dict4X4_50);
+            _detectorParameters = new DetectorParameters
+            {
+                AdaptiveThreshWinSizeMin = 3,
+                AdaptiveThreshWinSizeMax = 23,
+                AdaptiveThreshWinSizeStep = 10,
+                MinMarkerPerimeterRate = 0.03,
+                MaxMarkerPerimeterRate = 4.0
+            };
+
+        }
+
+        // ボタン1: カメラ起動と検出開始
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_isCameraRunning)
+            {
+                StopCamera();
+            }
+            else
+            {
+                StartCamera();
+            }
+        }
+
+        // カメラ起動処理
+        private void StartCamera()
+        {
+            _videoCapture = new VideoCapture(0);
+            if (!_videoCapture.IsOpened())
+            {
+                MessageBox.Show("カメラを開けませんでした。");
+                return;
+            }
+
+            _isCameraRunning = true;
+            Task.Run(() => CaptureFrames());
+        }
+
+        // カメラ停止処理
+        private void StopCamera()
+        {
+            _isCameraRunning = false;
+            _videoCapture?.Release();
+            pictureBox1.Image = null;
+        }
+
+        // フレームのキャプチャと表示
+        private void CaptureFrames()
+        {
+            while (_isCameraRunning)
+            {
+                using (var frame = new Mat())
+                {
+                    _videoCapture.Read(frame);
+                    if (frame.Empty())
+                        continue;
+
+                    using (Mat detectedFrame = DetectArucoMarkers(frame.Clone()))
+                    {
+                        Bitmap bitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(detectedFrame);
+                        Invoke(new Action(() =>
+                        {
+                            pictureBox1.Image?.Dispose();
+                            pictureBox1.Image = bitmap;
+                        }));
+                    }
+                }
+            }
+        }
+
+        // Arucoマーカーの検出と姿勢推定
+        private Mat DetectArucoMarkers(Mat frame)
+        {
+            Mat grayFrame = new Mat();
+            Cv2.CvtColor(frame, grayFrame, ColorConversionCodes.BGR2GRAY);
+
+            Point2f[][] corners;
+            int[] ids;
+            Point2f[][] rejectedCandidates;
+
+            CvAruco.DetectMarkers(
+                grayFrame,
+                _dictionary,
+                out corners,
+                out ids,
+                _detectorParameters,
+                out rejectedCandidates
+            );
+
+            if (ids != null && ids.Length > 0)
+            {
+                // 検出されたマーカーを描画
+                CvAruco.DrawDetectedMarkers(frame, corners, ids);
+
+                // カメラパラメータと歪み係数の設定
+                var cameraMatrix = InputArray.Create(new double[,]
+                {
+                    { 1000, 0, frame.Width / 2 },
+                    { 0, 1000, frame.Height / 2 },
+                    { 0, 0, 1 }
+                });
+
+                var distCoeffs = InputArray.Create(new double[] { 0, 0, 0, 0, 0 });
+
+                for (int i = 0; i < ids.Length; i++)
+                {
+                    // OutputArrayのインスタンスを作成するために、配列を使用します
+                    Mat rvec = new Mat();
+                    Mat tvec = new Mat();
+
+                    // 姿勢推定
+                    CvAruco.EstimatePoseSingleMarkers(
+                        new Point2f[][] { corners[i] }, // Point2f[][] 型に変換
+                        0.05f, // マーカーの実寸（メートル単位）
+                        cameraMatrix,
+                        distCoeffs,
+                        rvec,
+                        tvec
+                    );
+
+                    // マーカーの姿勢を描画
+                    DrawAxisManual(frame, cameraMatrix, distCoeffs, rvec, tvec, 0.1f);
+                }
+            }
+            return frame;
+        }
+
+        //マーカの姿勢を描画する関数
+        private void DrawAxisManual(Mat image, InputArray cameraMatrix, InputArray distCoeffs, Mat rvec, Mat tvec, float length)
+        {
+            // 座標軸の3Dポイントを定義
+            var axisPoints = new Mat(4, 1, MatType.CV_32FC3);
+            axisPoints.Set(0, new Point3f(0, 0, 0));       // 原点
+            axisPoints.Set(1, new Point3f(length, 0, 0));  // X軸
+            axisPoints.Set(2, new Point3f(0, length, 0));  // Y軸
+            axisPoints.Set(3, new Point3f(0, 0, -length)); // Z軸
+
+            // 2D投影されたポイントを格納する Mat を作成
+            var imagePoints = new Mat();
+
+            // 3Dポイントを画像平面上に投影
+            Cv2.ProjectPoints(
+                axisPoints,
+                rvec,
+                tvec,
+                cameraMatrix,
+                distCoeffs,
+                imagePoints
+            );
+
+            // 投影結果をPoint2fの配列に変換
+            Point2f[] points = new Point2f[imagePoints.Rows];
+            for (int i = 0; i < imagePoints.Rows; i++)
+            {
+                points[i] = imagePoints.At<Point2f>(i);
+            }
+
+            // Point2fからOpenCvSharp.Pointに明示的に変換して描画
+            Cv2.Line(image,
+                new OpenCvSharp.Point((int)points[0].X, (int)points[0].Y),
+                new OpenCvSharp.Point((int)points[1].X, (int)points[1].Y),
+                Scalar.Red, 2);   // X軸
+            Cv2.Line(image,
+                new OpenCvSharp.Point((int)points[0].X, (int)points[0].Y),
+                new OpenCvSharp.Point((int)points[2].X, (int)points[2].Y),
+                Scalar.Green, 2); // Y軸
+            Cv2.Line(image,
+                new OpenCvSharp.Point((int)points[0].X, (int)points[0].Y),
+                new OpenCvSharp.Point((int)points[3].X, (int)points[3].Y),
+                Scalar.Blue, 2);  // Z軸
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
